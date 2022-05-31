dd8d4e04c8f0db58b241b9d5335d7269de0783bc
Tim Herrin <teffluvium@gmail.com>
2022-05-30 23:05:48 -0700

    Adds personal and org repos to "clone_all_repos.sh"

--- git/clone_all_repos.sh
+++ git/clone_all_repos.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
 # Clone all specified repos
 
-cd ~/dev || return 1
+cd ~/Projects || return 1
+
+
 
 REPOS=(
   # Fill out repos here
-  "org/repo"
+  # "org/repo"
+  "Teffluvium/dotfiles_new"
+  "Teffluvium/my_boids"
+  "Teffluvium/CS50_ProblemSets"
+  "Teffluvium/DSP_py"
+  "Teffluvium/Quaternions_Py"
+  "Teffluvium/imageTraining_py"
+  "Teffluvium/Quaternians_cpp"
+  "Teffluvium/matlab_tools"
+  "Teffluvium/dot_vim"
+  "Teffluvium/projecteuler"
+  "Teffluvium/LaTeX-Docs"
+  "VorData/vorcoder"
+  "VorData/matlab_embedding"
+  "VorData/imageTraining"
+  "VorData/JPForensics-iOS"
+  "VorData/JPForensics-Android"
+  "VorData/vorcore"
+  "VorData/php_web_app"
 )
 
 for REPO in "${REPOS[@]}"
 do
   echo "----- CLONING ----- $REPO"
-	git clone git@github.com:"$REPO".git
+	git clone git@github.com:"$REPO".git --recursive
 done
