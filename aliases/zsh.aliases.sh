b3ceb8d1e61cd727c097c8e91ad676aa7ff6a3a5
Tim Herrin <teffluvium@gmail.com>
2022-05-30 17:42:16 -0700

    Adds alias for dirs='dirs -v'

--- aliases/zsh.aliases.sh
+++ aliases/zsh.aliases.sh
@@ -29,8 +29,10 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 alias ~='cd ~/'
+alias dirs='dirs -v'
 
 # Prompt if overwriting
+alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
