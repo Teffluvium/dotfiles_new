1548a75ad91aea8ad1b66669dda5dc0385fc58c9
Tim Herrin <teffluvium@gmail.com>
2022-06-21 15:41:14 -0700

    Adds conditional to check if system is MacOS

--- macos/install-homebrew.sh
+++ macos/install-homebrew.sh
@@ -2,6 +2,11 @@
 #
 # Install homebrew if it's not already installed
 
+if ! [ `uname` == 'Darwin' ]; then
+  echo "Not running MacOS.  Skipping Homebrew installation"
+  exit 0
+fi
+
 if ! type brew > /dev/null 2>&1; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
